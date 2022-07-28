691d27022fd9e38e0044caa9e0dbfab55cae8f22
Luiz Carlos <luiz.junior@globalsys.com.br>
2022-07-28 20:45:34 -0300

    coisas

--- src/app.builder.ts
+++ src/app.builder.ts
@@ -1,7 +1,6 @@
 import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { json } from 'express'
-import { LogExceptionFilter } from '@hialves-gbs/globalsys-modules'
 
 import { AppModule } from './app.module'
 
@@ -11,9 +10,7 @@ export async function appBuilder() {
   app.use(json({ limit: '50mb' }))
   process.env.NODE_ENV === 'production' &&
     app.useGlobalFilters(
-      new LogExceptionFilter('typeorm', 'commondb_write', 'log', {
-        enabledConsoleLog: true,
-      })
+   
     )
   app.useGlobalPipes(new ValidationPipe({ transform: true }))
 
